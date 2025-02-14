26b6d01e35b8b03d58c34f755f237b624bda3463
jjreder <jjreder@gmail.com>
2025-02-14 17:21:45 -0500

    modified neo6m to work

--- sensors/Neo6M.py
+++ sensors/Neo6M.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import asyncio
 import io
+import traceback
 from random import randint
 
 import pynmea2
@@ -41,16 +42,22 @@ class Neo6M(Sensor):
         :return: the data e.x. {"temperature": 34.7, "pressure": 106.4, "humidity": 0.56}
         """
 
-        while 1:
+        while True:
             try:
-                line = self.sio.readline()
-                msg = pynmea2.parse(line)
-                self.logger.critical(repr(msg))
-            except serial.SerialException as e:
-                self.logger.critical('Device error: {}'.format(e))
+                lines = self.sio.readlines()
+                for line in lines:
+                    try:
+                        msg = pynmea2.parse(line)
+                        data = msg.data
+                        self.logger.critical(repr(msg))
+                        if "lat" in data and "lon" in data and "altitude" in data:
+                            return {"latitude": data["lat"], "longitude": data["lon"], "altitude": data["altitude"]}
+
+                    except pynmea2.ParseError:
+                        self.logger.critical(f'Trying to parse data {line!r} error: {traceback.format_exc()}')
+                        continue
+            except serial.SerialException:
+                self.logger.critical(f'Failed to read: device error: {traceback.format_exc()}')
                 break
-            except pynmea2.ParseError as e:
-                self.logger.critical('Parse error: {}'.format(e))
-                continue
-        return {"finished": 1}
+
 
