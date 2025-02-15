1d493778cc0f1c33c24e6b6920d2b6d1cc9dde9d
jjreder <jjreder@gmail.com>
2025-02-15 11:12:33 -0500

    fixed Neo6M_SATS

--- sensors/Neo6M.py
+++ sensors/Neo6M.py
@@ -10,6 +10,7 @@ import pynmea2
 from Sensor import Sensor
 import serial
 
+current_satellites = None
 
 
 class Neo6M_GPS(Sensor):
@@ -44,7 +45,7 @@ class Neo6M_GPS(Sensor):
 
         :return: the data e.x. {"temperature": 34.7, "pressure": 106.4, "humidity": 0.56}
         """
-
+        global current_satellites
         while True:
             try:
                 line = self.sio.readline()
@@ -52,6 +53,8 @@ class Neo6M_GPS(Sensor):
                     self.logger.critical(f"Failed to read: no message received.")
                     break
                 msg = pynmea2.parse(line)
+                if msg.sentence_type == "GSV":  # satellites
+                    current_satellites = msg.num_sv_in_view
                 if msg.sentence_type == "GGA":  # Only type that contains lat, long, and alt
 
                     # .latitude and .longitude function as helpers
@@ -82,13 +85,6 @@ class Neo6M_SATS(Sensor):
          attributes and connections necessary to interface with the sensor
         :return: None
         """
-        self.port = "/dev/ttyAMA0"
-        try:
-            self.serial = serial.Serial(self.port, baudrate=9600, timeout=0.5)
-        except serial.SerialException:
-            raise FileNotFoundError(f"Neo6M serial port ({self.port}) not found. This is a critical error.")
-        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.serial, self.serial))
-        self.reader = pynmea2.NMEAStreamReader()
         super().__init__()
 
 
@@ -101,21 +97,6 @@ class Neo6M_SATS(Sensor):
         :return: the data e.x. {"temperature": 34.7, "pressure": 106.4, "humidity": 0.56}
         """
 
-        while True:
-            try:
-                line = self.sio.readline()
-                msg = pynmea2.parse(line)
-                if msg.sentence_type == "GGA":  # Only type that contains lat, long, and alt
-
-                    # .latitude and .longitude function as helpers
-                    return {"latitude": msg.latitude, "longitude": msg.longitude, "altitude": msg.altitude}
-
-            except pynmea2.ParseError:
-                self.logger.critical(f'Trying to parse data {line!r} error: {traceback.format_exc()}')
-                continue
-            except serial.SerialException:
-                self.logger.critical(f'Failed to read: device error: {traceback.format_exc()}')
-                break
-        return None  # None value indicates failure to read device
+        return current_satellites
 
 
